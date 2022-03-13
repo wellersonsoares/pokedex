b6ef11cbcf2825a9288e381aef751a087e732754
Wellerson Soares <101151576+wellersonsoares@users.noreply.github.com>
2022-03-13 17:40:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+//Esse código vai esconder o cartão que está aparecendo por padrão e fazer aparecer na tela o cartão selecionado no menu
+
+
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+        
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+
+    })
+})
